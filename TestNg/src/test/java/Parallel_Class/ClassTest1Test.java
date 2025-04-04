44111995b7b2f1535797baa31312b8c3bc08a95a
ypanditm17 <ypanditm17@gmail.com>
2025-04-04 15:22:33 +0530

    last

---
+++ TestNg/src/test/java/Parallel_Class/ClassTest1Test.java
@@ -0,0 +1,21 @@
+package Parallel_Class;
+
+import org.testng.annotations.Test;
+
+public class ClassTest1Test {
+
+  @Test
+  public void testATest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void testBTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void testCTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
